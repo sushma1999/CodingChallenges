c2b9eb158eb13a9b1ef63d8fb43e0177995cc06a
sushma1999 <56186986+sushma1999@users.noreply.github.com>
2020-03-25 16:39:10 +0530

    added Hashmap tp create a telephone directory

--- src/DictionaryMap.java
+++ src/DictionaryMap.java
@@ -2,7 +2,7 @@
 import java.util.*;
         import java.io.*;
 
-class DictionayMap{
+class DictionaryMap {
     public static void main(String []argh){
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
